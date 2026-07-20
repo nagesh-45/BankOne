164f3bce548434b7e4159c93cd48a224828e99dc
nageshkumargandla <nageshgandla.45@gmail.com>
2026-07-20 08:55:06 +0530

    Sprint 0: Initial Spring Boot project setup

---
+++ src/main/java/com/bankone/BankOneApplication.java
@@ -0,0 +1,13 @@
+package com.bankone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bankone/BankOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
